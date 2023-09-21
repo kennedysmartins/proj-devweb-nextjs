8964579abb60329a40a15aeef5fbb249a218a795
Sergio Henrique <shs_28@hotmail.com>
2023-09-21 20:59:14 -0300

    Cria Appbar, Drawer e refatora page

--- src/app/components/Appbar.js
+++ src/app/components/Appbar.js
@@ -1,27 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link';
 
-function Appbar() {
+function Appbar({onMenuToggle}) {
   return (
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-     <Image
-      src="/logo.png"
-      width={200}
-      height={500}
-      alt="Picture of the author"
-    />
-     </a>
-     <div className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-       <Link href="/post" className="mr-5">Home</Link>
-       <Link href="/" className="mr-5">Home</Link>
-       <Link href="/" className="mr-5">Home</Link>
-       <Link href="/" className="mr-5">Home</Link>
-     </div>
-     <Link href="/login">
-        <button className="inline-flex items-center bg-yellow-500 border-0 py-1 px-3 mt-4 md:mt-0">Login</button>
-     </Link>
-  </div>
+    <div className='flex justify-between items-center bg-gray-700 p-4'>
+      <div className='text-white'>My App</div>
+      <button onClick={onMenuToggle}>
+        <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+      </button>
+    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
+'use client'
 import Appbar from './components/Appbar';
 import Bottom from './components/Bottom';
+import Drawer from './components/Drawer';
+import React, {useState} from 'react';
 
 export default function Home() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState();
+
+  const handleMenuToggle  = () => {
+    setIsDrawerOpen(!isDrawerOpen)
+  }
+  
   return (
     <main className="min-h-screen">
-      <Appbar></Appbar>
+      <Appbar onMenuToggle={handleMenuToggle}></Appbar>
+      <Drawer isOpen={isDrawerOpen} onClose={handleMenuToggle}></Drawer>
 
       <Bottom></Bottom>
     </main>
